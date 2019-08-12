275f8cc63a928ee9844ba09695b46f0cc52eaf1e
李靖峰 <lijingfeng@lijingfengdeiMac.local>
2019-08-13 01:01:13 +0800

    支持固定长度

--- randname.go
+++ randname.go
@@ -5,38 +5,79 @@ import (
 	"fmt"
 )
 
-var defaultSequence int32 = math.MinInt32
+var (
+	defaultSequence uint32 = 0
+	defaultLength int = 10
+)
 
-func pseudoEncrypt(seed int32) int32 {
+func pseudoEncrypt(seed uint32, bits uint) uint32 {
 	var (
-		l1 = (seed >> 16) & 0xffff
-		r1 = seed & 0xffff
-		l2, r2 int32
+		mask uint32 = (1<<bits) -1
+		l1 = (seed >> bits) & mask
+		r1 = seed & mask
+		l2, r2 uint32
 	)
 
 	for i := 0; i < 3; i++ {
 		l2 = r1
-		r2 = l1 ^ int32(math.Round((float64((1366*r1 + 150889) % 714025) / 714025.0) * 32767.0))
+		r2 = l1 ^ (uint32(math.Round((float64((1366*r1 + 150889) % 714025) / 714025.0) * 32767.0)) & mask)
 		l1 = l2
 		r1 = r2
 	}
-	return (r1 << 16) + l1
+	return (r1 << bits) + l1
 }
 
 type Generator struct{
-	seq int32
+	seq uint32
+	length int
+	bits uint
 }
 
-func (x *Generator) SetSeq(seq int32) {
+func (x *Generator) SetSeq(seq uint32) {
 	x.seq = seq
 }
 
 func (x *Generator) Next() string {
-	n := pseudoEncrypt(x.seq)
+	n := pseudoEncrypt(x.seq, x.bits)
 	x.seq++
-	return fmt.Sprintf("%d", uint32(n))
+
+	format := fmt.Sprintf("%%.%dd", x.length) //字符串前补0格式
+	return fmt.Sprintf(format, uint32(n))
 }
 
-func New() *Generator{
-	return &Generator{seq: defaultSequence}
+//New 创建一个唯一ID生成器，可指定数字最大长度，但需要注意，支持的sequence数值范围为 [0, MaxSeq()]
+func New(length int) *Generator{
+	if length > 10 || length < 1{ //限制最大最小长度
+		length = defaultLength
+	}
+	
+	bits := getBits(int(math.Pow10(length)-1))
+	// fmt.Println("math.Pow10(length)-1: ", math.Pow10(length)-1)
+	// fmt.Println("new bits: ", bits)
+	
+	return &Generator{
+		seq: defaultSequence,
+		length: length,
+		bits: bits,
+	}
+}
+
+//MaxSeq 返回可用的最大sequence值
+func (x *Generator) MaxSeq() uint32 {
+	return (1 << (x.bits*2)) -1
+}
+
+//getBits 返回 (一个整数占用多少二进制位) / 2
+func getBits(n int) (bits uint) {
+	var x int
+	var i uint
+	for ; i < 64; i++{
+		y := x | (1 << i)
+		if y > n {
+			break
+		}
+		x = y
+	}
+	bits = i/2
+	return
 }
